196c50352280adfe17dccf8a6661b8b802b5b771
Alex Tran <tranduclong092@gmail.com>
2024-06-18 09:16:09 +0300

    Feat: create main function and placeholder for helper functions

--- main.py
+++ main.py
@@ -5,6 +5,27 @@ An message encryption/description script which use a master password to hide any
 """
 
 def main():
-  print("Hello world!")
+  print("+-------------------------------------------+")
+  print("|       Let's encrypt some messages!        |")
+  print("+-------------------------------------------+")
+
+  prompt = "0"
+  while (prompt != "3"):
+    prompt = input("Type 1 to encrypt, 2 to decrypt. Type 3 to end the program: ")
+    match prompt:
+      case "1":
+        encrypt()
+      case "2":
+        decrypt()
+      case "3":
+        print("Goodbye!")
+      case _:
+        print("Wrong command, try again!")
+
+def encrypt():
+  print("encrypt")
+
+def decrypt():
+  print("decrypt")
 
 __name__ == "__main__" and main()
