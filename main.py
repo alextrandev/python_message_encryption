a3164d6151b78ed33b2d6c267c4e897ad40cde3b
Alex Tran <tranduclong092@gmail.com>
2024-06-18 09:06:46 +0300

    Feat: create the main script

---
+++ main.py
@@ -0,0 +1,10 @@
+"""
+File: main.py
+-------------------
+An message encryption/description script which use a master password to hide any message
+"""
+
+def main():
+  print("Hello world!")
+
+__name__ == "__main__" and main()
