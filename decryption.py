1b0254814e6af91aae0c505f10b72b16852e55e7
Alex Tran <tranduclong092@gmail.com>
2024-06-18 09:29:30 +0300

    Refactor: change password into message, add space character

--- decryption.py
+++ decryption.py
@@ -1,23 +1,23 @@
 """
 File: decryption.py
 -------------------
-Decrypt an encrypted password with a master password
+Decrypt an encrypted message with a master password
 """
 
-CHARACTERS = ['a', 'o', '3', 'b', 'p', 'L', 'e', 'u', 'k', 'G', 'q', 'f', 'N', 'j', 'W', 'X', '8', '6', '9', 'F', 'J', 'S', 's', '7', 'm', 'P', 'D', '2', 'I', 'E', 'C', 'K', '5', '0', 'R', 'd', 'h', 'H', 'n', 'r', 'B', 'V', 'T', 'c', 'v', 'Y', 'i', 'g', 'y', 'Q', 'M', 'A', 'U', '1', 't', 'z', 'w', 'Z', 'x', 'O', '4', 'l']
+CHARACTERS = ['a', 'o', '3', 'b', 'p', 'L', 'e', ' ', 'u', 'k', 'G', 'q', 'f', 'N', 'j', 'W', 'X', '8', '6', '9', 'F', 'J', 'S', 's', '7', 'm', 'P', 'D', '2', 'I', 'E', 'C', 'K', '5', '0', 'R', 'd', 'h', 'H', 'n', 'r', 'B', 'V', 'T', 'c', 'v', 'Y', 'i', 'g', 'y', 'Q', 'M', 'A', 'U', '1', 't', 'z', 'w', 'Z', 'x', 'O', '4', 'l']
 
 def main():
-  input_pwd = input("Input your encrypted password: ")
+  input_msg = input("Input your encrypted message: ")
   master_pwd = input("What's your master password? ")
-  decrypted_pwd = decrypt(input_pwd, master_pwd)
-  print(f"Your original password is {decrypted_pwd}")
+  decrypted_msg = decrypt(input_msg, master_pwd)
+  print(f"Your original message is {decrypted_msg}")
 
-def decrypt(input_pwd, master_pwd):
+def decrypt(input_msg, master_pwd):
   master_pwd_values =  map_to_value(master_pwd)
-  input_pwd_values = map_to_value(input_pwd)
-  decrypted_pwd_values = subtract_values(input_pwd_values, master_pwd_values)
-  decrypted_pwd = map_to_char(decrypted_pwd_values)
-  return decrypted_pwd
+  input_msg_values = map_to_value(input_msg)
+  decrypted_msg_values = subtract_values(input_msg_values, master_pwd_values)
+  decrypted_msg = map_to_char(decrypted_msg_values)
+  return decrypted_msg
 
 def map_to_value(string):
   return list(map(lambda i: CHARACTERS.index(i), string)) 
--- encryption.py
+++ encryption.py
@@ -1,23 +1,23 @@
 """
 File: encryption.py
 -------------------
-Encrypt a password using a master password
+Encrypt a message using a master password
 """
 
-CHARACTERS = ['a', 'o', '3', 'b', 'p', 'L', 'e', 'u', 'k', 'G', 'q', 'f', 'N', 'j', 'W', 'X', '8', '6', '9', 'F', 'J', 'S', 's', '7', 'm', 'P', 'D', '2', 'I', 'E', 'C', 'K', '5', '0', 'R', 'd', 'h', 'H', 'n', 'r', 'B', 'V', 'T', 'c', 'v', 'Y', 'i', 'g', 'y', 'Q', 'M', 'A', 'U', '1', 't', 'z', 'w', 'Z', 'x', 'O', '4', 'l']
+CHARACTERS = ['a', 'o', '3', 'b', 'p', 'L', 'e', ' ', 'u', 'k', 'G', 'q', 'f', 'N', 'j', 'W', 'X', '8', '6', '9', 'F', 'J', 'S', 's', '7', 'm', 'P', 'D', '2', 'I', 'E', 'C', 'K', '5', '0', 'R', 'd', 'h', 'H', 'n', 'r', 'B', 'V', 'T', 'c', 'v', 'Y', 'i', 'g', 'y', 'Q', 'M', 'A', 'U', '1', 't', 'z', 'w', 'Z', 'x', 'O', '4', 'l']
 
 def main():
-  input_pwd = input("Input your normal password to be encrypt: ")
+  input_msg = input("Input your message to be encrypt: ")
   master_pwd = input("What's your master password? ")
-  encrypted_pwd = encrypt(input_pwd, master_pwd)
-  print(f"Your encrypted password is {encrypted_pwd}. Keep your master password safe for future decryption")
+  encrypted_msg = encrypt(input_msg, master_pwd)
+  print(f"Your encrypted message is {encrypted_msg}. Keep your master password safe for future decryption")
 
-def encrypt(input_pwd, master_pwd):
+def encrypt(input_msg, master_pwd):
   master_pwd_values =  map_to_value(master_pwd)
-  input_pwd_values = map_to_value(input_pwd)
-  encrypted_pwd_values = combine_values(input_pwd_values, master_pwd_values)
-  encrypted_pwd = map_to_char(encrypted_pwd_values)
-  return encrypted_pwd
+  input_msg_values = map_to_value(input_msg)
+  encrypted_msg_values = combine_values(input_msg_values, master_pwd_values)
+  encrypted_msg = map_to_char(encrypted_msg_values)
+  return encrypted_msg
 
 def map_to_value(string):
   return list(map(lambda i: CHARACTERS.index(i), string)) 
